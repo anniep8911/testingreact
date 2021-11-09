daad06e891062fc0062dfbf4b9b9e625b28e6c7c
anniep8911 <pjatopazz@gmail.com>
2021-11-10 02:52:32 +0900

    commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="hdrWrap">
+        <header>
+          <h1>logohere</h1>
+          <nav>
+            <h2>sub01</h2>
+            <h2>sub02</h2>
+            <h2>sub03</h2>
+          </nav>
+        </header>
+      </div>
+      <div className="mnWrap">
+        <main>
+          
+        </main>
+      </div>
+      <div className="cntWrap">
+        <div className="content cnt01">
+          <header>
+            <h2>content title</h2>
+          </header>
+          <section>
+            <article>
+              <div className="image i1"></div>
+              <div className="texts">
+                <h3>segemnt title</h3>
+                <h5>Lorem ipsum dolor sit amet.</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In aliquam commodi aspernatur excepturi dignissimos blanditiis quas officiis impedit qui quis sapiente perspiciatis, accusantium dolorum possimus eveniet cumque repudiandae sint eum.</p>
+              </div>
+            </article>
+            <article>
+              <div className="image i2"></div>
+              <div className="texts">
+                <h3>segemnt title</h3>
+                <h5>Lorem ipsum dolor sit amet.</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In aliquam commodi aspernatur excepturi dignissimos blanditiis quas officiis impedit qui quis sapiente perspiciatis, accusantium dolorum possimus eveniet cumque repudiandae sint eum.</p>
+              </div>
+            </article>
+            <article>
+              <div className="image i3"></div>
+              <div className="texts">
+                <h3>segemnt title</h3>
+                <h5>Lorem ipsum dolor sit amet.</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In aliquam commodi aspernatur excepturi dignissimos blanditiis quas officiis impedit qui quis sapiente perspiciatis, accusantium dolorum possimus eveniet cumque repudiandae sint eum.</p>
+              </div>
+            </article>
+            <article>
+              <div className="image i4"></div>
+              <div className="texts">
+                <h3>segemnt title</h3>
+                <h5>Lorem ipsum dolor sit amet.</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In aliquam commodi aspernatur excepturi dignissimos blanditiis quas officiis impedit qui quis sapiente perspiciatis, accusantium dolorum possimus eveniet cumque repudiandae sint eum.</p>
+              </div>
+            </article>
+            <article>
+              <div className="image i5"></div>
+              <div className="texts">
+                <h3>segemnt title</h3>
+                <h5>Lorem ipsum dolor sit amet.</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In aliquam commodi aspernatur excepturi dignissimos blanditiis quas officiis impedit qui quis sapiente perspiciatis, accusantium dolorum possimus eveniet cumque repudiandae sint eum.</p>
+              </div>
+            </article>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
